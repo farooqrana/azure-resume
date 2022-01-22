55eebe99e3fc0e94b267dfa8b2760cb83d077830
Muhammad Farooq <farooqrana@hotmail.com>
2022-01-23 01:15:26 +0500

    Updates to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,17 @@
+Window.addEventListener('DONContentLoaded')
+getVisitCount();
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById ("counter").innerText = count;
+    }) .catch{function(error){
+        console.log(error);
+    }};
+    return count
+    }
